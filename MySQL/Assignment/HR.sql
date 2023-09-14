901d9085d3d66ad580ace54fd07d3ba9d3568faf
gitkinjal <divine.kinjal@gmail.com>
2023-09-14 18:10:18 +0530

    Update HR.sql

--- MySQL/Assignment/HR.sql
+++ MySQL/Assignment/HR.sql
@@ -122,11 +122,11 @@ Group By Manager_ID;
 Select Manager_ID, Count(Employee_ID) as 'Count of Employees', Department_ID*/
 
 /* Answer Q.11) : Select the employee name, department id, and the salary. Group the result with the
-manager name and the employee last name should have second letter 'a!'
+manager name and the employee last name should have second letter 'a'.
 Select concat(First_Name, ' ', Last_Name) as 'Employee Name', Department_ID, Salary
 From emp
 Group By Manager_ID
-And Last_Name Like '%a!';*/
+And Where Last_Name Like '_a%';*/
 
 /* Answer Q.12) : Display the average of sum of the salaries and group the result with the department id.
 Order the result with the department id.
@@ -153,5 +153,86 @@ Case
     End as 'Commission Result'
     From emp
     Order By Employee_ID;
+    
+/* Part-3 */
+/* Answer Q.1) : Write a query that displays the employee's last names only from the string's 2-5th position with the first letter capitalized and all other 
+letters lowercase, Give each column an appropriate label.*/
+
+
+
+/* Answer Q.2): Write a query that displays the employee's first name and last name along with a " in between for e.g.: first name : Ram; 
+last name : Kumar then Ram-Kumar. Also displays the month on which the employee has joined.  */
+Select concat_ws("-", First_Name,Last_Name) as 'Employee-Name' ,
+Date_Format(Hire_Date,"%b") as 'Joined in Month'
+from emp
+Where First_Name Like '%a%'
+and Last_Name Like '%a%';
+
+/* Answer Q.3) : Write a query to display the employee's last name and if half of the salary is greater than ten thousand then increase 
+the salary by 10% else by 11.5% along with the bonus amount of 1500 each. Provide each column an appropriate label.*/
+Select Last_Name,
+Case
+	When Salary > 10000 Then Salary + 0.10
+    Else Salary + 0.115 + 1500
+    End As Increment
+From emp;
+
+/* Answer Q.4) : Display the employee ID by Appending two zeros after 2nd digit and 'E' in the end, department id, salary and 
+the manager name all in Upper case, if the Manager name consists of 'z' replace it with '$!   */
+
+
+/* Answer Q.5) : Write a query that displays the employee's last names with the first letter capitalized and all other letters lowercase, 
+and the length of the names, for all employees whose name starts with J, A, or M. Give each column an appropriate label. Sort the results 
+by the employees' last names.*/
+
+
+
+/* Answer Q.6) : Create a query to display the last name and salary for all employees. Format the salary to be 15 characters long, left-padded with $. 
+Label the column SALARY. */
+
+
+
+/* Answer Q.7) : Display the employee's name if it is a palindrome.*/
+
+
+
+/* Answer Q.8) : Display First names of all employees with initcaps.*/
+
+
+
+/* Answer Q.9) : From LOCATIONS table, extract the word between first and second space from the STREET ADDRESS column.*/
+
+
+
+/* Answer Q.10) : Extract first letter from First Name column and append it with the Last Name. Also add "@systechusa.com" at the end.
+Name the column as e-mail address. All characters should be in lower case. Display this along with their First Name. */
+
+
+
+/* Answer Q.11) : Display the names and job titles of all employees with the same job as Trenna. */
+
+
+/* Answer Q.12) : Display the names and department name of all employees working in the same city as Trenna. */
+
+
+
+
+/* Answer Q.13) : Display the name of the employee whose salary is the lowest.*/
+
+
+
+/* Answer Q.14) : Display the names of all employees except the lowest paid. */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
